6718c9852cbd4fa921dd5fa0c28451f816ad297a
beaesreal <beaesreal@gmail.com>
2022-09-29 19:10:57 +0000

    Exercise of Domain Generator done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+*/
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        //console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`);
+        let div = document.createElement("div");
+        div.innerText = `
+        ${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}
+        `;
+        document.body.appendChild(div);
+      }
+    }
+  }
+}
