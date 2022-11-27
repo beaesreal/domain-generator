6ad65c9564046c6fb1b369ecacbc47e592762bae
beaesreal <beaesreal@gmail.com>
2022-11-27 13:11:39 +0000

    Exercise fixed

--- src/app.js
+++ src/app.js
@@ -1,33 +1,59 @@
-/* eslint-disable 
-import "bootstrap";
-import "./style.css";
+/* eslint-disable */
+const { pronouns, adjetives, nouns, tlds } = require("./variables");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function applyDomainHack(domain) {
+  let counter = 0;
+  let domainHacks = [];
+  for (let i = 0; i < tlds.length; i++) {
+    const tld = tlds[i];
+    let j = domain.length - 1;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    for (let k = tld.length - 1; k >= 0; k--) {
+      const tldCharacter = tld[k];
+      if (domain[j] === tldCharacter) {
+        counter++;
+        j--;
+      }
+    }
+    if (counter === tld.length) {
+      const domainSliced = domain.slice(0, domain.length - tld.length);
+      const domainHack = domainSliced + "." + tld;
+      domainHacks.push(domainHack);
+    } else {
+      domainHacks.push(domain + "." + tld);
+    }
 
+    counter = 0;
+  }
 
-*/
+  return domainHacks;
+}
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let extension = [".com", ".net", ".us"];
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let l = 0; l < extension.length; l++) {
-        //console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`);
-        let div = document.createElement("div");
-        div.innerText = `
-        ${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}
-        `;
-        document.body.appendChild(div);
+function domainGenerator() {
+  let domains = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    const pronoun = pronouns[i];
+    for (let j = 0; j < adjetives.length; j++) {
+      const adjetive = adjetives[j];
+      for (let k = 0; k < nouns.length; k++) {
+        const noun = nouns[k];
+        const domainWithoutTLD = pronoun + adjetive + noun;
+        const domain = applyDomainHack(domainWithoutTLD);
+        domains.push(domain);
       }
     }
   }
+
+  return domains;
 }
+
+function printDomains(domainsInput, numberOfDomainsToPrint) {
+  const length = numberOfDomainsToPrint
+    ? numberOfDomainsToPrint
+    : domainsInput.length;
+  for (let i = 0; i < length; i++) {
+    console.log(domainsInput[i]);
+  }
+}
+
+printDomains(domainGenerator());
