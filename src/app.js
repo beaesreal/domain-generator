bbd9457240f8909f37c3402e3da53a7d430a9c9b
beaesreal <beaesreal@gmail.com>
2022-12-02 12:44:08 +0000

    Exercise fixed and completed

--- src/app.js
+++ src/app.js
@@ -1,59 +1,15 @@
-/* eslint-disable */
 const { pronouns, adjetives, nouns, tlds } = require("./variables");
 
-function applyDomainHack(domain) {
-  let counter = 0;
-  let domainHacks = [];
-  for (let i = 0; i < tlds.length; i++) {
-    const tld = tlds[i];
-    let j = domain.length - 1;
-
-    for (let k = tld.length - 1; k >= 0; k--) {
-      const tldCharacter = tld[k];
-      if (domain[j] === tldCharacter) {
-        counter++;
-        j--;
-      }
-    }
-    if (counter === tld.length) {
-      const domainSliced = domain.slice(0, domain.length - tld.length);
-      const domainHack = domainSliced + "." + tld;
-      domainHacks.push(domainHack);
-    } else {
-      domainHacks.push(domain + "." + tld);
-    }
-
-    counter = 0;
-  }
-
-  return domainHacks;
-}
-
-function domainGenerator() {
-  let domains = [];
-  for (let i = 0; i < pronouns.length; i++) {
-    const pronoun = pronouns[i];
-    for (let j = 0; j < adjetives.length; j++) {
-      const adjetive = adjetives[j];
-      for (let k = 0; k < nouns.length; k++) {
-        const noun = nouns[k];
-        const domainWithoutTLD = pronoun + adjetive + noun;
-        const domain = applyDomainHack(domainWithoutTLD);
-        domains.push(domain);
+var domainNameGenerator = (a, b, c, d) => {
+  for (let w = 0; w < a.length; w++) {
+    for (let x = 0; x < b.length; x++) {
+      for (let y = 0; y < c.length; y++) {
+        for (let z = 0; z < d.length; z++) {
+          console.log(a[w] + b[x] + c[y] + d[z]);
+        }
       }
     }
   }
+};
 
-  return domains;
-}
-
-function printDomains(domainsInput, numberOfDomainsToPrint) {
-  const length = numberOfDomainsToPrint
-    ? numberOfDomainsToPrint
-    : domainsInput.length;
-  for (let i = 0; i < length; i++) {
-    console.log(domainsInput[i]);
-  }
-}
-
-printDomains(domainGenerator());
+domainNameGenerator(pronouns, adjetives, nouns, tlds);
